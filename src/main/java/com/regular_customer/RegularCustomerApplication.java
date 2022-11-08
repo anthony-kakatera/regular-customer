fa28b513295edf38cc74c5c45db137c5dcfc4e3d
Anthony Kakatera <anthonyckakatera@gmail.com>
2022-11-08 12:23:32 +0200

    Added SpringApplicationBuilder method

--- src/main/java/com/regular_customer/RegularCustomerApplication.java
+++ src/main/java/com/regular_customer/RegularCustomerApplication.java
@@ -2,11 +2,17 @@ package com.regular_customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
 public class RegularCustomerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RegularCustomerApplication.class);
+    }
 
     public static void main(final String[] args) {
         SpringApplication.run(RegularCustomerApplication.class, args);
