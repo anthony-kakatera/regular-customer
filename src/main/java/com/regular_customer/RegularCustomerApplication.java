b4007c2908295de6a6bafa182e5b0dd2c4265b50
Anthony Kakatera <anthonyckakatera@gmail.com>
2022-11-08 12:16:50 +0200

    Added (extends SpringBootServletInitializer) to help with initialization on .War file

--- src/main/java/com/regular_customer/RegularCustomerApplication.java
+++ src/main/java/com/regular_customer/RegularCustomerApplication.java
@@ -2,10 +2,11 @@ package com.regular_customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class RegularCustomerApplication {
+public class RegularCustomerApplication extends SpringBootServletInitializer {
 
     public static void main(final String[] args) {
         SpringApplication.run(RegularCustomerApplication.class, args);
